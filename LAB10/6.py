b1f7d38855abade7b8e9db4fca03935cedf3d408
PP2 21B050614 <90209380+PP2-21B050614@users.noreply.github.com>
2022-05-11 23:24:36 +0600

    Delete LAB10 directory

--- LAB10/6.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(dbname='phone', user='phone', password='password)', host='localhost')
-cur = conn.cursor()
-cur.execute("DELETE FROM phonebook where name='Zora'")
-conn.commit()
-conn.close()
-cur.close()
